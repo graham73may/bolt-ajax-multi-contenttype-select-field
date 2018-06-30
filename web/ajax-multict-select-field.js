513863983e86e665d2b7db4f30eb344bcd55dad4
Graham May <graham.may@soapbox.co.uk>
2018-06-30 16:52:39 +0100

    Making $select relative. Also added support for custom branding paths. #2

--- web/ajax-multict-select-field.js
+++ web/ajax-multict-select-field.js
@@ -2,71 +2,70 @@
     'use strict';
 
     $.fn.extend({
-        customSelect2Sortable: function () {
+        customSelect2Sortable : function () {
             var $select = $(this);
-
-            var ul = $select.next('.select2-container').first('ul.select2-selection__rendered');
+            var ul      = $select.next('.select2-container').first('ul.select2-selection__rendered');
 
             ul.sortable({
-                items    : 'li:not(.select2-search)',
-                tolerance: 'pointer',
-
-                stop: function () {
+                items     : 'li:not(.select2-search)',
+                tolerance : 'pointer',
+                stop      : function () {
                     $($(ul).find('.select2-selection__choice').get().reverse()).each(function () {
-                        var id = $(this).data('data').id;
+                        var id     = $(this).data('data').id;
                         var option = $(this).find('option[value="' + id + '"]')[0];
 
                         $(this).prepend(option);
                     });
                 }
             });
-
         }
     });
 
     function modifySelect2 () {
         var $buicSelects = $('.js-ajax-multi-ct-select');
-        var len = $buicSelects.length;
-        var options = {};
+        var len          = $buicSelects.length;
+        var options      = {};
         var i;
         var $buicSelect;
         var $select;
 
         for (i = 0; i < len; i++) {
             $buicSelect = $($buicSelects[i]);
-            $select = $buicSelect.find('select');
-            options = $select.data('select2').options.options;
+            $select     = $buicSelect.find('select');
+            options     = $select.data('select2').options.options;
 
             $select.select2({
-                language               : {
-                    errorLoading: function () {
+                language                : {
+                    errorLoading : function () {
                         return 'Searching...';
                     }
                 },
-                placeholder            : options.placeholder,
-                allowClear             : options.allowClear || true,
-                minimumResultsForSearch: options.minimumResultsForSearch,
-                width                  : options.width,
-                ajax                   : {
-                    select        : $select,
-                    url           : function (params) {
-                        var page = params.page || 1;
+                placeholder             : options.placeholder,
+                allowClear              : options.allowClear || true,
+                minimumResultsForSearch : options.minimumResultsForSearch,
+                width                   : options.width,
+                ajax                    : {
+                    select         : $select,
+                    url            : function (params) {
+                        var page         = params.page || 1;
+                        var $container   = $(this.select).closest('.js-ajax-multi-ct-select-container');
+                        var brandingPath = $container.data('branding-path');
 
-                        return '/bolt/ajax-multi-ct-select?page=' + page;
+                        return brandingPath + '/ajax-multi-ct-select?page=' + page;
                     },
-                    dataType      : 'json',
-                    delay         : 500,
-                    type          : 'POST',
-                    data          : function (params) {
-                        $select = $(this);
-                        var $container = $select.closest('.js-ajax-multi-ct-select-container');
+                    dataType       : 'json',
+                    delay          : 500,
+                    type           : 'POST',
+                    data           : function (params) {
+                        var $container = $(this.select).closest('.js-ajax-multi-ct-select-container');
+
                         return {
-                            q    : params.term, // search term
-                            page : params.page || 1,
-                            field: $container.data('field')
+                            q     : params.term, // search term
+                            page  : params.page || 1,
+                            field : $container.data('field')
                         };
                     },
-                    processResults: function (data, params) {
+                    processResults : function (data, params) {
                         // parse the results into the format expected by Select2
                         // since we are using custom formatting functions we do not need to
                         // alter the remote JSON data, except to indicate that infinite
@@ -74,13 +73,13 @@
                         params.page = params.page || 1;
 
                         return {
-                            results   : data.results,
-                            pagination: {
-                                more: (data.pager.current * data.pager.limit) < data.pager.count
+                            results    : data.results,
+                            pagination : {
+                                more : (data.pager.current * data.pager.limit) < data.pager.count
                             }
                         };
                     },
-                    cache         : false
+                    cache          : false
                 }
             });
 
@@ -89,9 +88,7 @@
             }
 
             $buicSelect.find('.select2-selection__placeholder').html('(none)');
-
         }
-
     }
 
     $('.repeater-add').on('click', function () {
@@ -99,5 +96,4 @@
     });
 
     $(window).on('load', modifySelect2);
-
 }(jQuery));
